0a782eab858a192ac8f58c859f8b69891b4742c3
nCoding <nkt.coding@gmail.com>
2021-06-30 21:54:45 +0530

    Link for latest video

--- KalleHalldenNewVideo/KHytWeb.py
+++ KalleHalldenNewVideo/KHytWeb.py
@@ -8,21 +8,14 @@ if test.status_code ==200:
     print("Website exists")
 elif test.status_code == 404:
     print("No website found")
-
 soup = BeautifulSoup(test.content, 'html5lib')
 scrapped_web = soup.prettify()
-
-'''
-with open("KalleHalldenNewVideo/webdetails.txt","w") as wd:
-    wd.write(scrapped_web)
-    wd.close()
-#file = open("webdetails.txt","r")
-#k = file.read()
-'''
-
 r = re.findall(r'\"title\":.*?,\"webPageType\":',soup.prettify())
+link = re.findall(r'\"/watch.*?\"',r[1])
+newVideoLink = link[0].replace('"','')
+print('https://www.youtube.com'+newVideoLink)
 
-print(r[1])
+
 
 
 
